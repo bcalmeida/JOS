87052e22ba9b74be55fef5b6566f958c29f4b2b1
Bruno Correia Almeida <bcalm@mit.edu>
2014-11-10 16:59:43 -0500

    Implement alloc_block in fs/fs.c (L5-Ex3)
    Exercise 3. Use free_block as a model to implement alloc_block, which should find a free disk block in the bitmap, mark it used, and return the number of that block. When you allocate a block, you should immediately flush the changed bitmap block to disk with flush_block, to help file system consistency.
    
    Use make grade to test your code. Your code should now pass "alloc_block"

--- fs/fs.c
+++ fs/fs.c
@@ -62,7 +62,21 @@ alloc_block(void)
 	// super->s_nblocks blocks in the disk altogether.
 
 	// LAB 5: Your code here.
-	panic("alloc_block not implemented");
+	if (!super)
+		panic("in alloc_block: super not initialized");
+
+	// Loop through all possible blocks, checking if there is any free
+	int blockno;
+	for (blockno = 1; blockno < super->s_nblocks; blockno++) {
+		// If find a free block, mark it used and flush the bitmap block
+		if(block_is_free(blockno)) {
+			bitmap[blockno / 32] &= ~(1<<(blockno%32));
+			flush_block(&bitmap[blockno/32]);
+			return blockno;
+		}
+	}
+
+	// No block is free
 	return -E_NO_DISK;
 }
 
