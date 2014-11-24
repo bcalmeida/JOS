dcfcf44a935159b27cd9ca3537361d5da5205e0d
Bruno Correia Almeida <bcalm@mit.edu>
2014-11-24 16:18:28 -0500

    Implement PCI initialization function for E1000 and map it's MMIO (L6-Ex2,3,4)
    Exercise 3 statement: "Implement an attach function to initialize the E1000. Add an entry to the pci_attach_vendor array in kern/pci.c to trigger your function if a matching PCI device is found (be sure to put it before the {0, 0, 0} entry that mark the end of the table). You can find the vendor ID and device ID of the 82540EM that QEMU emulates in section 5.2. You should also see these listed when JOS scans the PCI bus while booting.
    
    For now, just enable the E1000 device via pci_func_enable. We'll add more initialization throughout the lab.
    
    We have provided the kern/e1000.c and kern/e1000.h files for you so that you do not need to mess with the build system. They are currently blank; you need to fill them in for this exercise. You may also need to include the e1000.h file in other places in the kernel.
    
    When you boot your kernel, you should see it print that the PCI function of the E1000 card was enabled. Your code should now pass the pci attach test of make grade."
    
    Exercise 4 statement: "In your attach function, create a virtual memory mapping for the E1000's BAR 0 by calling mmio_map_region (which you wrote in lab 4 to support memory-mapping the LAPIC).
    
    You'll want to record the location of this mapping in a variable so you can later access the registers you just mapped. Take a look at the lapic variable in kern/lapic.c for an example of one way to do this. If you do use a pointer to the device register mapping, be sure to declare it volatile; otherwise, the compiler is allowed to cache values and reorder accesses to this memory.
    
    To test your mapping, try printing out the device status register (section 13.4.2). This is a 4 byte register that starts at byte 8 of the register space. You should get 0x80080783, which indicates a full duplex link is up at 1000 MB/s, among other things."

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,26 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
 
-// LAB 6: Your driver code here
+// Pointer to the start of E1000's MMIO region
+volatile uint32_t *e1000;
+
+// Initialize the E1000, which is a PCI device
+// Returns 0 on success (always)
+int
+attach_e1000(struct pci_func *pcif)
+{
+	// Negotiates an MMIO region with the E1000 and stores
+	// its base and size in BAR 0 (that is, reg_base[0] and reg_size[0])
+	// Also fills irq_line with the interrupt line allocated for it
+	pci_func_enable(pcif);
+
+	// Map MMIO to an appropiate virtual memory
+	physaddr_t pa = pcif->reg_base[0];
+	size_t size = pcif->reg_size[0];
+	e1000 = mmio_map_region(pa, size);
+
+	// Test MMIO by accessing one of it's registers
+	cprintf("E1000 MMIO test: Device Status Reg = %x (should be 80080783)\n",
+		*(e1000 + 2)); // E1000_STATUS offset is 8
+	return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,16 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include <kern/pci.h>
+
+// Keys for the E1000 82540EM, the one used by QEMU
+#define E1000_VENDOR_ID	0x8086
+#define E1000_DEV_ID 	0x100E
+
+// Registers
+#define E1000_STATUS	0x00008  /* Device Status - RO */
+
+// Functions headers
+int attach_e1000(struct pci_func *pcif);
+
 #endif	// JOS_KERN_E1000_H
