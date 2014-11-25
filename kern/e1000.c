2afcc21097ddf1894ab39f267dc5979013833d93
Bruno Correia Almeida <bcalm@mit.edu>
2014-11-25 15:53:57 -0500

    Implement E1000's transmission initialization and transmit_packet function (L6-Ex5,6)
    Code was heavily refactored.
    
    Exercises statements:
    
    Exercise 5. Perform the initialization steps described in section 14.5 (but not its subsections). Use section 13 as a reference for the registers the initialization process refers to and sections 3.3.3 and 3.4 for reference to the transmit descriptors and transmit descriptor array.
    
    Be mindful of the alignment requirements on the transmit descriptor array and the restrictions on length of this array. Since TDLEN must be 128-byte aligned and each transmit descriptor is 16 bytes, your transmit descriptor array will need some multiple of 8 transmit descriptors. However, don't use more than 64 descriptors or our tests won't be able to test transmit ring overflow.
    
    For the TCTL.COLD, you can assume full-duplex operation. For TIPG, refer to the default values described in table 13-77 of section 13.4.34 for the IEEE 802.3 standard IPG (don't use the values in the table in section 14.5).
    
    Exercise 6. Write a function to transmit a packet by checking that the next descriptor is free, copying the packet data into the next descriptor, and updating TDT. Make sure you handle the transmit queue being full.
    
    To test:
    Run make E1000_DEBUG=TXERR,TX qemu to run your test. You should see something like
    
    e1000: index 0: 0x271f00 : 9000002a 0
    ...

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,8 +1,140 @@
+#include <inc/memlayout.h>
+#include <inc/error.h>
+#include <inc/string.h>
+
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
-// Pointer to the start of E1000's MMIO region
-volatile uint32_t *e1000;
+#define NUM_TX_DESC 16
+
+volatile uint32_t *e1000; // Pointer to the start of E1000's MMIO region
+char *buffers[NUM_TX_DESC];
+struct tx_desc *tx_ring;
+
+/* Auxiliary Funcions */
+static int e1000_page_alloc(void *va, int perm);
+static physaddr_t va2pa(void *va);
+
+/* Testing Functions */
+static void check_mmio(int check);
+static void check_mappings(int check);
+static void show_mappings(int check);
+static void test_transmission();
+
+// Initializes transmision
+void
+init_transmission(void)
+{
+	// TODO: Throw the MMIOLIM logic to e1000_page_alloc
+	cprintf("E1000 initializing transmission\n");
+
+	/* Data structures setups */
+	// Allocate memory for descriptor ring
+	char *va = (char *) MMIOLIM;
+	int r;
+	if((r = e1000_page_alloc(va, PTE_P | PTE_W)) < 0)
+		panic("e1000_page_alloc: %e", r);
+
+	// Make descriptor ring point to that va
+	tx_ring = (struct tx_desc *) va;
+
+	// Allocate memory for the buffers (16)
+	int i;
+	for (i = 0; i < NUM_TX_DESC; i++) {
+		va += PGSIZE;
+		if((r = e1000_page_alloc(va, PTE_P | PTE_W)) < 0)
+			panic("e1000_page_alloc: %e", r);
+		buffers[i] = va;
+	}
+
+	// Test mappings
+	check_mappings(1);
+	show_mappings(1);
+
+	// Initial settings of the tx_descriptors
+	for (i = 0; i < NUM_TX_DESC; i++) {
+		// Set CMD.RS, so it updates DD
+		tx_ring[i].cmd |= E1000_TXD_CMD_RS;
+		// Set STATUS.DD, so it starts available
+		tx_ring[i].status |= E1000_TXD_STAT_DD;
+	}
+
+	/* Registers setup (as shown in chapter 14.5 of intel manual) */
+
+	// TDBAH & TDBAL (Transmit Descriptor Ring address)
+	// Always store physical address, not the virtual address!
+	// E1000_REG(E1000_TDBAH) = 0;
+	E1000_REG(E1000_TDBAL) = va2pa(tx_ring);
+
+	// TDLEN (Transmit Descriptor Ring length in bytes)
+	// Length is NUM_TX_DESC * sizeof(struct tx_desc), which is NUM_TX_DESC * 16
+	E1000_REG(E1000_TDLEN) = NUM_TX_DESC * sizeof(struct tx_desc);
+
+	// TDH & TDT (Transmit Decriptor Ring Head and Tail)
+	E1000_REG(E1000_TDH) = 0;
+	E1000_REG(E1000_TDT) = 0;
+
+	// TCTL (Transmit Control Register)
+	// Enable TCTL.EN
+	E1000_REG(E1000_TCTL) |= E1000_TCTL_EN;
+	// Enable TCTL.PSP
+	E1000_REG(E1000_TCTL) |= E1000_TCTL_PSP;
+	// Configure TCTL.CT
+	/* No need */
+	// Configure TCTL.COLD for full duplex operation (40h)
+	E1000_REG(E1000_TCTL) &= (~E1000_TCTL_COLD | 0x00040000); // Hard coded...
+
+	// TIPG
+	E1000_REG(E1000_TIPG) = 0;
+	E1000_REG(E1000_TIPG) += (10 <<  0); // TIPG.IPGT = 10, bits 0-9
+	E1000_REG(E1000_TIPG) += ( 4 << 10); // TIPG.IPGR1 = 4 (2/3*IPGR2), 10-19
+	E1000_REG(E1000_TIPG) += ( 6 << 20); // TIPG.IPGR2 = 6, bits 20-29
+}
+
+// Transmit packet
+void
+transmit_packet(void *buf, size_t size)
+{
+	// Initial checkings
+	if (size > 1518)
+		panic("Packet size is bigger than the maximum allowed");
+	if (!buf)
+		panic("Null pointer passed");
+
+	// Retrieve tail and check if it is available (if ring is not full)
+	// by checking if TXD.STATUS.DD is set
+	uint32_t tail = E1000_REG(E1000_TDT);
+	if (!(tx_ring[tail].status & 0x01)) {
+		// Drop packet if tx_ring is full
+		cprintf("tx_ring[tail] DD is not set: tx_ring is full. "
+			"Transmission aborted.\n");
+		return;
+	}
+
+	// Set tx_desc registers
+	// Set CMD.EOP, meaning this is the end of packet (Really needed?)
+	tx_ring[tail].cmd |= E1000_TXD_CMD_EOP;
+	// Set STAT.DD to 0, meaning this tx_desc is not done, and needs to be sent
+	tx_ring[tail].status &= ~E1000_TXD_STAT_DD;
+
+	// Put packet data in buffer
+	memset(buffers[tail], 0, PGSIZE);
+	memmove(buffers[tail], buf, size);
+
+	// Update tx descriptor
+	tx_ring[tail].addr = (uint64_t) va2pa(buffers[tail]);
+	tx_ring[tail].length = (uint16_t) size;
+
+	// Debugging
+	//cprintf("Transmitting packet from buf=%p, with size=%d\n", buf, size);
+	//cprintf("tx_ring[tail].addr = %p\n", tx_ring[tail].addr);
+	//cprintf("tx_ring[tail].length = %d\n", tx_ring[tail].length);
+
+	// Update tail
+	tail = (tail + 1) % NUM_TX_DESC;
+	E1000_REG(E1000_TDT) = tail;
+}
+
 
 // Initialize the E1000, which is a PCI device
 // Returns 0 on success (always)
@@ -19,8 +151,119 @@ attach_e1000(struct pci_func *pcif)
 	size_t size = pcif->reg_size[0];
 	e1000 = mmio_map_region(pa, size);
 
-	// Test MMIO by accessing one of it's registers
-	cprintf("E1000 MMIO test: Device Status Reg = %x (should be 80080783)\n",
-		*(e1000 + 2)); // E1000_STATUS offset is 8
+	check_mmio(1);
+
+	init_transmission();
+	test_transmission();
 	return 0;
+}
+
+/* --------------------------- */
+/* --- Auxiliary Functions --- */
+/* --------------------------- */
+
+// Custom page allocator for the E1000
+// Returns:
+//   0 on success
+//   -E_INVAL if va is in user space
+//   -E_NO_MEM if there is no more page to allocate a page or page table for mapping
+static int
+e1000_page_alloc(void *va, int perm)
+{
+	// Check if va is the kernel address space, so it is copied
+	// to every environment
+	if ((uint32_t) va < UTOP)
+		return -E_INVAL;
+
+	// Tries to allocate a physical page
+	struct PageInfo *pp = page_alloc(ALLOC_ZERO);
+	if (!pp) {
+	        return -E_NO_MEM;
+	}
+
+	// Tries to map the physical page at va of kern_pgdir
+	int r = page_insert(kern_pgdir, pp, va, perm);
+	if (r < 0) {
+	        page_free(pp);
+	        return -E_NO_MEM;
+	}
+	return 0;
+}
+
+// Translates virtual address to physical address
+// Could also use UVPT, but I think this way is simpler/easier
+static physaddr_t
+va2pa(void *va)
+{
+	struct PageInfo *pp = page_lookup(kern_pgdir, va, NULL);
+	if (!pp)
+		panic("va2pa: va is not mapped");
+	return page2pa(pp);
+}
+
+/* ------------------------- */
+/* --- Testing Functions --- */
+/* ------------------------- */
+static void
+check_mmio(int check)
+{
+	if (!check) return;
+	// Test MMIO by accessing one of it's status register
+	assert(E1000_REG(E1000_STATUS) == 0x80080783);
+
+	cprintf("E1000 MMIO is ok\n");
+}
+
+static void
+check_mappings(int check)
+{
+	if (!check) return;
+
+	// Check that the tx_ring page is present, writable and filled with zeroes
+	char *va = (char *) tx_ring;
+	*va = 'a';
+	assert(*(va)      ==  'a');
+	assert(*(va+10)   == '\0');
+	assert(*(va+100)  == '\0');
+	assert(*(va+1000) == '\0');
+	*va = 0;
+
+	// Check that buffers pages are all present, writable and filled with
+	// zeroes, by comparing it to the tx_ring page
+	int i;
+	for (i = 0; i < NUM_TX_DESC; i++) {
+		assert(memcmp(tx_ring, buffers[i], PGSIZE) == 0);
+	}
+
+	cprintf("E1000 TX mappings are ok\n");
+}
+
+static void
+show_mappings(int check)
+{
+	if (!check) return;
+
+	// Print all the TX mappings
+	cprintf("E1000 TX mappings:\n");
+	cprintf("\ttx_ring       \tva=%p, \tpa=%p\n", tx_ring, va2pa(tx_ring));
+	int i;
+	for (i = 0; i < NUM_TX_DESC; i++) {
+		cprintf("\tbuffers[%d] \tva=%p, \tpa=%p\n",
+			i, buffers[i], va2pa(buffers[i]));
+	}
+}
+
+static void
+test_transmission(void)
+{
+	uint32_t data[50];
+	int i;
+
+	for (i = 0; i < 50; i++) {
+		data[i] = i;
+	}
+
+	for (i = 0; i < 18; i++) {
+		transmit_packet(&data, sizeof(data));
+	}
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -3,14 +3,64 @@
 
 #include <kern/pci.h>
 
+// Macro to access the registers in MMIO
+#define E1000_REG(offset) *(e1000 + (offset>>2))
+
 // Keys for the E1000 82540EM, the one used by QEMU
-#define E1000_VENDOR_ID	0x8086
-#define E1000_DEV_ID 	0x100E
+#define E1000_VENDOR_ID 0x8086
+#define E1000_DEV_ID    0x100E
 
-// Registers
-#define E1000_STATUS	0x00008  /* Device Status - RO */
+/* Registers offsets in MMIO */
+#define E1000_STATUS   0x00008  /* Device Status - RO */
 
-// Functions headers
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+
+/* Transmit Control */
+#define E1000_TCTL_RST    0x00000001    /* software reset */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_BCE    0x00000004    /* busy check enable */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+#define E1000_TCTL_SWXOFF 0x00400000    /* SW Xoff transmission */
+#define E1000_TCTL_PBE    0x00800000    /* Packet Burst Enable */
+#define E1000_TCTL_RTLC   0x01000000    /* Re-transmit on late collision */
+#define E1000_TCTL_NRTU   0x02000000    /* No Re-transmit on underrun */
+#define E1000_TCTL_MULR   0x10000000    /* Multiple request support */
+
+/* Transmit Descriptor bit definitions */
+#define E1000_TXD_CMD_EOP  0x01
+#define E1000_TXD_CMD_RS   0x08
+#define E1000_TXD_STAT_DD  0x01
+
+/* Functions headers */
 int attach_e1000(struct pci_func *pcif);
+
+/* Structures */
+
+// Transmit descriptor
+// 63            48 47   40 39   32 31   24 23   16 15             0
+// +---------------------------------------------------------------+
+// |                         Buffer address                        |
+// +---------------+-------+-------+-------+-------+---------------+
+// |    Special    |  CSS  | Status|  Cmd  |  CSO  |    Length     |
+// +---------------+-------+-------+-------+-------+---------------+
+struct tx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+};
 
 #endif	// JOS_KERN_E1000_H
